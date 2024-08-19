851226ca4e158a8c53b72c46506df20a8ae1cd41
Michal <michal.rozga@yahoo.com>
2024-08-19 22:04:04 +0200

    Adjusted the firebase config.
    - Edited the App.js to house the new component

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAe4YeKNDi_IJbn-2pmeRk5_4rWYLSS05I",
-  authDomain: "scheduler-8f1ce.firebaseapp.com",
-  projectId: "scheduler-8f1ce",
-  storageBucket: "scheduler-8f1ce.appspot.com",
-  messagingSenderId: "543331455561",
-  appId: "1:543331455561:web:525b7dc8c2a4b443eae598",
-  measurementId: "G-2MXBSZ3XYP"
-};
-
+    apiKey: "AIzaSyAe4YeKNDi_IJbn-2pmeRk5_4rWYLSS05I",
+    authDomain: "scheduler-8f1ce.firebaseapp.com",
+    projectId: "scheduler-8f1ce",
+    storageBucket: "scheduler-8f1ce.appspot.com",
+    messagingSenderId: "543331455561",  
+    appId: "1:543331455561:web:525b7dc8c2a4b443eae598",  
+    measurementId: "G-2MXBSZ3XYP"  
+  };
+  
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
